79c0561cfc6a1aa5c8348cf8cfb57005b58128b7
chittipong <sminko1599@gmail.com>
2019-10-17 10:48:17 +0700

    edit app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var moment = require('moment');
+moment.locale("th");
+var app = express();
+app.use(express.static("www"));
+
+
+app.get("/", welcome);
+app.get("/today", showToday)
+
+app.listen('3000');
+
+function welcome(request, response){
+    response.send("<h1>Express by Korawit</h1>");
+}
+function showToday(request, response){
+    console.log(moment().format("LLL"));
+    response.send(moment().format("LLL"));
+}
